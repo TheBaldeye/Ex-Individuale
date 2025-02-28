69409e88e163924b417a4610b2276c7c2cb6d9cd
TheBaldeye <gianluca.gabrielli@hotmail.it>
2025-02-28 14:59:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Creazione nuovo Rettangolo di partenza:");
+            Rettangolo rettangoloBase = new Rettangolo();
+            Console.WriteLine($"\nL'altezza è: {rettangoloBase.Altezza}");
+            Console.WriteLine($"\nLa larghezza è: {rettangoloBase.Larghezza}");
+
+            Console.WriteLine($"\nCreazione nuovo Rettangolo personalizzato:");
+            Rettangolo rettangoloPersonalizzato = new Rettangolo(20, 30);
+            Console.WriteLine($"\nL'altezza è: {rettangoloPersonalizzato.Altezza}");
+            Console.WriteLine($"\nLa larghezza è: {rettangoloPersonalizzato.Larghezza}");
+
+            Console.WriteLine($"\nCreazione nuovo Rettangolo personalizzato 1:");
+            Rettangolo rettangoloPersonalizzato1 = new Rettangolo(30, 40);
+            Console.WriteLine($"\nL'altezza è: {rettangoloPersonalizzato1.Altezza}");
+            Console.WriteLine($"\nLa larghezza è: {rettangoloPersonalizzato1.Larghezza}");
+
+            Console.WriteLine($"\nL'area del rettangolo personalizzato è: {rettangoloPersonalizzato.CalcoloArea()}");
+            Console.WriteLine($"\nL'area del rettangolo personalizzato moltiplicato per 8 è: {rettangoloPersonalizzato.CalcoloArea(8)}");
+
+            Console.WriteLine($"\nI rettangoli creati sono: {Rettangolo.ConteggioRettangoli}");
+
+            Console.WriteLine($"\n{Rettangolo.ConfrontaAree(rettangoloPersonalizzato, rettangoloPersonalizzato1).CalcoloArea()}");
+            
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Rettangolo
+    {
+        public double Altezza { get; set; }
+        public double Larghezza { get; set; }
+
+        public static int ConteggioRettangoli {get; set;}
+
+
+        /*public Rettangolo(double altezza = 1, double larghezza = 1) // all in one
+        {
+            Altezza = altezza;
+            Larghezza = larghezza;
+            ConteggioRettangoli ++;
+        }  */      
+        public Rettangolo( double altezza , double larghezza)
+        {
+            Altezza = altezza;
+            Larghezza = larghezza;
+            ConteggioRettangoli++;
+        }
+
+        public Rettangolo() : this(1, 1)
+        {
+            
+        }
+     
+        public double CalcoloArea()
+        {                     
+            return this.Altezza * this.Larghezza;
+        }
+
+        public double CalcoloArea(int n)
+        {
+            return this.Altezza * this.Larghezza * n;
+        }
+
+        public static Rettangolo ConfrontaAree(Rettangolo r1, Rettangolo r2)
+        {
+            if (r1.CalcoloArea() < r2.CalcoloArea())
+            {
+                return r2;
+            }
+            else
+            {
+                return r1;
+            }
+        }
+    }
+}
